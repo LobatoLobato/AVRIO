f76bc5445be374735b10f886aed4c24734e56121
FelipeLobato <felipelobato.rj@gmail.com>
2022-12-16 21:40:37 -0300

    Changed lib version checking

--- lib-version-check/lib-version-check.js
+++ lib-version-check/lib-version-check.js
@@ -1,15 +1,27 @@
-const fs = require("fs");
-const path = "./lib/AVRIO/library.json";
-const prevPath = "./lib-version-check/prev.txt";
-const versionRegexp = /(?<=("version":\s*"))[0-9\.]+/;
-const content = fs.readFileSync(path).toString();
-const version = content.match(versionRegexp).at(0);
-const prevVersion = fs.readFileSync(prevPath).toString();
-if (version === prevVersion) {
-  throw new Error(
-    "Library version is the same as previous version, please update the version in " +
-      path
-  );
-}
-fs.writeFileSync(prevPath, version);
-console.log("Library version check completed");
+const { exec } = require("child_process");
+const { readFileSync } = require("fs");
+const LOCAL_PATH = "./lib/AVRIO/library.json";
+const LOCAL_VERSION_REGEXP = /(?<=("version":\s*"))[0-9\.]+/;
+const LOCAL_VERSION = readFileSync(LOCAL_PATH)
+  .toString()
+  .match(LOCAL_VERSION_REGEXP)
+  .at(0);
+
+exec("pio pkg show lobatolobato/AVRIO", (error, stdout, stderr) => {
+  if (error) throw new Error(error.message);
+
+  if (stdout) {
+    const VERSIONS = stdout.match(/(?<=version(.|\n|\r)*)\d+\.\d+\.\d+/gim);
+    const CURRENT_VERSION = VERSIONS.at(0);
+    if (LOCAL_VERSION === CURRENT_VERSION) {
+      const error = `Published version (${CURRENT_VERSION}) is the same as local version (${LOCAL_VERSION})
+      Please update the version in ${LOCAL_PATH}`;
+      throw new Error(error);
+    } else if (LOCAL_VERSION < CURRENT_VERSION) {
+      const error = `Published version (${CURRENT_VERSION}) is higher than local version (${LOCAL_VERSION})
+      Please update the version in ${LOCAL_PATH}`;
+      throw new Error(error);
+    }
+    console.log("Library version check completed");
+  }
+});
